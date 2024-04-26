2a4fe13f9f1c14a2bdb1ef4881b868628dc3de48
lissandro07 <144491928+lissandro07@users.noreply.github.com>
2024-04-25 23:17:35 -0600

    Update CM22118.pl

--- CM22118Prolog/CM22118.pl
+++ CM22118Prolog/CM22118.pl
@@ -1,7 +1,7 @@
 saldo_cuenta(mario, 1000).
 saldo_cuenta(flor, 3000000).
 saldo_cuenta(antonia, 20000).
-
+// TODO Auto-generated method stub
 es_millonaria(Persona) :-
     saldo_cuenta(Persona, Saldo),
     Saldo > 1000000.
